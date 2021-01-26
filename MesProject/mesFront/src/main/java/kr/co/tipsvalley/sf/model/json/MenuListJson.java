043d92ce1df103628b07703d08e3933c01a5ba63
dRocks <dRocks@DESKTOP-LTCNI0H.mshome.net>
2021-01-27 00:05:05 +0900

    파일 재정의함

---
+++ MesProject/mesFront/src/main/java/kr/co/tipsvalley/sf/model/json/MenuListJson.java
@@ -0,0 +1,35 @@
+package kr.co.tipsvalley.sf.model.json;
+
+import java.util.ArrayList;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MenuListJson {
+    @JsonProperty("MenuList")
+    private ArrayList<MenuInfoRealJson> MenuList;
+
+	public ArrayList<MenuInfoRealJson> getMenuList() {
+		return MenuList;
+	}
+
+	public void setMenuInfoRealJsonList(ArrayList<MenuInfoRealJson> menuInfoRealJsonList) {
+		this.MenuList = menuInfoRealJsonList;
+	}
+	@JsonProperty("SubMenuList")
+	private ArrayList<MenuInfoRealJson> MenuList1;
+
+	public ArrayList<MenuInfoRealJson> getMenuList1() {
+			return MenuList1;
+	}
+
+	public void setMenuInfoRealJsonList1(ArrayList<MenuInfoRealJson> menuInfoRealJsonList) {
+		this.MenuList1 = menuInfoRealJsonList;
+	}
+	
+    
+}
