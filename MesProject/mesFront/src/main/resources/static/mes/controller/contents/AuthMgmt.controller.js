9fd9cbaf6288e182b9183f09f942d551fed26d8c
rocks <rocks@LAPTOP-NA4DNRNG>
2021-01-27 14:47:31 +0900

    기준관리 관련 메뉴 하위메뉴 생성

---
+++ MesProject/mesFront/src/main/resources/static/mes/controller/contents/AuthMgmt.controller.js
@@ -0,0 +1,31 @@
+sap.ui.define([
+	"tips/mes/controller/BaseController",
+	'sap/ui/core/mvc/Controller',
+    'sap/ui/model/BindingMode',
+    'sap/ui/model/json/JSONModel'
+], function(BaseController, Controller, BindingMode, JSONModel) {
+    "use strict";
+        
+    return BaseController.extend("tips.mes.controller.contents.AuthMgmt",
+    {
+        onInit: function() {
+        	window.contents3 = this;
+        },
+        
+        onActive: function() {
+        	console.log("tips.mes.controller.contents.Contents3 onActive()");
+        },
+        
+        errorback: function(){ },
+        
+        callbackFunction : function(oModel) {
+            var oData = oModel.getProperty("/result/list");
+            this.byId("idTable").setModel(new JSONModel(oData));
+        },
+              
+        errorCallbackFunction : function() {
+            console.log("error callback");
+        }
+        
+    });
+}, true);
